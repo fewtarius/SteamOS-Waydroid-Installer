2e682e2a8947014c72708073f6277b2c485a46df
fewtarius <fewtarius@steamfork.org>
2024-07-09 23:50:30 +0000

    Fix up dynamic resolution support.

--- bin/android_launcher.sh
+++ bin/android_launcher.sh
@@ -27,17 +27,20 @@ killall -9 cage >/dev/null 2>&1
 sudo /usr/bin/waydroid-container-stop
 sudo /usr/bin/waydroid-container-start
 
+export MY_RESOLUTION="$(fbwidth)x$(fbheight)"
+echo "Resolution: ${MY_RESOLUTION}"
+
 # Check if non Steam SHORTCUT has the game / app as the launch option
 if [ -z "$1" ]
 then
 	# launch option not provided. launch Waydroid via cage and show the full ui right away
-	cage -- bash -c 'wlr-randr --output X11-1 --custom-mode $(fbheight)x$(fbwidth)@60Hz ; \
+	cage -- bash -c 'wlr-randr --output X11-1 --custom-mode ${MY_RESOLUTION}@60Hz ; \
 			/usr/bin/waydroid show-full-ui $@ & \
 			sleep 15 ; \
 			sudo /usr/bin/waydroid-fix-controllers '
 else
 	# launch option provided. launch Waydroid via cage but do not show full ui yet
-	cage -- bash -c 'wlr-randr --output X11-1 --custom-mode $(fbheight)x$(fbwidth)@60Hz ; \
+	cage -- bash -c 'wlr-randr --output X11-1 --custom-mode ${MY_RESOLUTION}@60Hz ; \
 			/usr/bin/waydroid session start $@ & \
 			sleep 15; \
 			sudo /usr/bin/waydroid-fix-controllers ; \
