4aa67d758e040c22ed7e80c5e414e0645e4103e6
fewtarius <fewtarius@steamfork.org>
2025-04-10 10:30:39 +0000

    lzip is required

--- extras/waydroid-post-update.sh
+++ extras/waydroid-post-update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -Sy --noconfirm --overwrite '*' fbset wlroots weston wlr-randr cage lxc python-gbinder python-gobject nftables dnsmasq gtk3 dbus-python waydroid
+sudo pacman -Sy --noconfirm --overwrite '*' fbset wlroots weston wlr-randr cage lxc python-gbinder python-gobject nftables dnsmasq gtk3 dbus-python waydroid lzip
